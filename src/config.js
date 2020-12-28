4dabe68cc41b69adcc6aec4d80ddb70b7ac25e82
John Wellbrock <wellbrock23john@gmail.com>
2020-12-28 09:28:48 -0700

    init commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	PORT: process.env.PORT || 8000,
+	NODE_ENV: process.env.NODE_ENV || "development",
+	DATABASE_URL:
+		process.env.DATABASE_URL ||
+		"postgresql://postgres@localhost/yourDatabaseNameHere",
+	API_TOKEN: process.env.API_TOKEN || "dummy-api-token",
+	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+};
