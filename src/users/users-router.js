33252e2e465d0fd8dedf280eddeec343599ad7e8
John Wellbrock <wellbrock23john@gmail.com>
2021-01-05 23:47:24 -0500

    my way

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -7,53 +7,51 @@ const userRouter = express.Router();
 const jsonBodyParser = express.json();
 
 userRouter.post("/", jsonBodyParser, async (req, res, next) => {
-	const { password, email, first_name, last_name } = req.body;
+  const { password, email, first_name, last_name } = req.body;
 
-	for (const field of ["first_name", "last_name", "email", "password"])
-		if (!req.body[field])
-			return res.status(400).json({
-				error: `Missing '${field}' in request body`,
-			});
+  for (const field of ["first_name", "last_name", "email", "password"])
+    if (!req.body[field])
+      return res.status(400).json({
+        error: `Missing '${field}' in request body`,
+      });
 
-	try {
-		const passwordError = UserService.validatePassword(password);
+  try {
+    const passwordError = UserService.validatePassword(password);
 
-		if (passwordError) return res.status(400).json({ error: passwordError });
+    if (passwordError) return res.status(400).json({ error: passwordError });
 
-		const hasUserWithEmail = await UserService.hasUserWithEmail(
-			req.app.get("db"),
-			email
-		);
+    const hasUserWithEmail = await UserService.hasUserWithEmail(
+      req.app.get("db"),
+      email
+    );
 
-		if (hasUserWithEmail)
-			return res.status(400).json({ error: `Email already in use` });
+    if (hasUserWithEmail)
+      return res.status(400).json({ error: `Email already in use` });
 
-		const hashedPassword = await UserService.hashPassword(password);
+    const hashedPassword = await UserService.hashPassword(password);
 
-		const newUser = {
-			email,
-			password: hashedPassword,
-			first_name,
-			last_name,
-		};
+    const newUser = {
+      email,
+      password: hashedPassword,
+      first_name,
+      last_name,
+    };
 
-		const user = await UserService.insertUser(req.app.get("db"), newUser);
+    const user = await UserService.insertUser(req.app.get("db"), newUser);
 
-		res
-			.status(201)
-			.location(path.posix.join(req.originalUrl, `/${user.id}`))
-			.json(UserService.serializeUser(user));
-	} catch (error) {
-		next(error);
-	}
+    res
+      .status(201)
+      .location(path.posix.join(req.originalUrl, `/${user.id}`))
+      .json(UserService.serializeUser(user));
+  } catch (error) {
+    next(error);
+  }
 });
 
-userRouter.get("/", jsonBodyParser, requireAuth, (req, res, next) => {
-	UserService.getUserWithEmail(req.app.get("db"), req.user.email)
-		.then((user) => {
-			res.json(UserService.serializeUser(user));
-		})
-		.catch(next);
-});
+userRouter.get("/", requireAuth, (req, res, next) => {
+  res.json(req.user);
+  next();
+})
+;
 
 module.exports = userRouter;
