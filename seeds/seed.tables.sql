925c5a17ff375f1b0c4ef28f1824d8f6f8749521
John Wellbrock <wellbrock23john@gmail.com>
2021-01-07 19:46:07 -0500

    updated seed file

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -12,28 +12,13 @@ VALUES
     'The Admin',
     -- password = "pass"
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
-  ),
-  (2,
-    'admin1@admin.com',
-    'Jake',
-    'The Snake',
-    -- password = "pass"
-    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
-  ),
-  (
-    3,
-    'admin2@admin.com',
-    'My',
-    'Dad',
-    -- password = "pass"
-    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
 
 INSERT INTO "vehicles" ("id", "year", "make", "model", "submodel", "color", "paintCondition", "coverage", "userId")
 VALUES
   (1, 2010, 'Ford', 'Focus', 'SE', 'Blue', '3', 'Partial Front', 1),
-  (2, 2019, 'Honda', 'CRV', null, 'Silver', '7', 'Danger Zone. No Coverage.', 3),
-  (3, 2021, 'Porsche', '911', 'Turbo S Touring', 'Slate Grey', '10', 'Full Body', 2);
+  (2, 2019, 'Honda', 'CRV', null, 'Silver', '7', 'Danger Zone. No Coverage.', 1),
+  (3, 2021, 'Porsche', '911', 'Turbo S Touring', 'Slate Grey', '10', 'Full Body', 1);
 
 -- because we explicitly set the id fields
 -- update the sequencer for future automatic id setting
